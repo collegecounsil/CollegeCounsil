bcab6b5a4581a658406d1beaf811759def569be6
Raj <Raj@Raj-PC>
2018-04-20 00:14:45 +0530

    Registration mail

--- src/main/java/com/student/corner/SC/service/impl/RegistrationServiceImpl.java
+++ src/main/java/com/student/corner/SC/service/impl/RegistrationServiceImpl.java
@@ -2,10 +2,18 @@ package com.student.corner.SC.service.impl;
 
 import java.net.URLEncoder;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.mail.internet.MimeMessage;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import com.student.corner.SC.bean.CommonBO;
@@ -18,6 +26,23 @@ import com.student.corner.SC.util.Utility;
 
 @Service("RegistrationService")
 public class RegistrationServiceImpl implements RegistrationService {
+
+	private final Log LOGGER = LogFactory.getLog(this.getClass());
+	
+	@Value("${application.url}")
+    private String APPLICATION_URL;
+	
+	@Value("${spring.mail.username}")//Gmail block emails if username and from name is differ (for server other then Gmail)
+    private String MAIL_FROM;
+	
+	@Value("${user.registration.link.expire.limit}")
+    private String USER_REGISTRATION_LINK_EXPIRE_LIMIT;
+	
+	@Value("${user.registration.email.template.subject}")
+    private String USER_REGISTRATION_EMAIL_TEMPLATE_SUBJECT;
+	
+	@Value("${user.registration.email.template.body}")
+    private String USER_REGISTRATION_EMAIL_TEMPLATE_BODY;
 	
 	@Autowired
 	private JavaMailSender mailSender;
@@ -34,11 +59,42 @@ public class RegistrationServiceImpl implements RegistrationService {
 	@Override
 	public CommonBO sendVerificationMailToUser(String email) {
 		CommonBO commonBO = new CommonBO();
-		User user = userService.findUserByEmail(email);
-		if(user !=null){ 
-//			String key = URLEncoder.encode(utility.encrypt(user.getId() + utility.SEPARATOR + new Date().getTime()), "UTF-8");
+		try {
+			User user = userService.findUserByEmail(email);
+			if(user !=null){
+				String key = URLEncoder.encode(utility.encrypt(user.getId() + utility.SEPARATOR + new Date().getTime()), "UTF-8");
+				String dataURL = this.APPLICATION_URL + "register/verifyUser?key=" + key;
+				Map<String,String> replacers = new HashMap<String, String>();
+		        replacers.put("[name]", user.getFirstName() != null ? user.getFirstName() : user.getEmail());
+		        replacers.put("[url]", dataURL);
+		        replacers.put("[minutes]", this.USER_REGISTRATION_LINK_EXPIRE_LIMIT);
+		        
+		        String emailTemplate = this.USER_REGISTRATION_EMAIL_TEMPLATE_BODY;
+		        
+		        String emailBody = utility.replaceText(emailTemplate, replacers);
+				
+		        MimeMessage msg = mailSender.createMimeMessage();
+		        MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+		        helper.setFrom(this.MAIL_FROM);
+		        helper.setTo(email);
+		        msg.setSubject(this.USER_REGISTRATION_EMAIL_TEMPLATE_SUBJECT);
+		        msg.setContent(emailBody, "text/html");
+		
+		        mailSender.send(msg);
+		        commonBO.setStatus("success");
+				commonBO.setMessage(messageSource.getMessage("user.registration.success", new String[] { email} , "Registration successful", null));
+			}
+			else{
+				commonBO.setStatus("error");
+				commonBO.setMessage(messageSource.getMessage("user.notFound.error", new String[] { email} , "User not found", null));
+			}
+		} catch(Exception e){
+			
+			LOGGER.error("Exception in sending mail to user: " + e);
+			commonBO.setStatus("error");
+			commonBO.setMessage(messageSource.getMessage("user.verification.mail.error", null , "Error in sending email ", null));
 		}
-		return null;
+		return commonBO;
 	}
 
 	@Override
--- src/main/java/com/student/corner/SC/util/Utility.java
+++ src/main/java/com/student/corner/SC/util/Utility.java
@@ -1,17 +1,40 @@
 package com.student.corner.SC.util;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
 import java.util.UUID;
 import java.util.regex.Pattern;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.codec.Base64;
 import org.springframework.stereotype.Service;
 
 import com.student.corner.SC.util.Constants.KeyType;
 
 @Service("utility")
 public class Utility {
+	
+	@Value("${application.private.key}")
+	public String APP_PRIVATE_KEY;//
+	
+	private final Log LOGGER = LogFactory.getLog(this.getClass());
+	
+	public static final String algorithm = "DES";
 	
 	//Date Formats to be used in Application
 	public static final String DATE_FORMAT_MMddyyyy_SLASH = "MM/dd/yyyy";
@@ -64,5 +87,73 @@ public class Utility {
 	
 	public Boolean matchPasswordPolicy(final String password) {
 		return pattern.matcher(password).matches();
+	}
+	
+	public String encrypt(String stringToBeEncrypted) {
+		String encryptedString = null;
+		try {
+			DESKeySpec dks = new DESKeySpec(this.APP_PRIVATE_KEY.getBytes());
+			SecretKeyFactory skf = SecretKeyFactory.getInstance(Utility.algorithm);
+			SecretKey desKey = skf.generateSecret(dks);
+			Cipher cipher = Cipher.getInstance(Utility.algorithm);
+			cipher.init(Cipher.ENCRYPT_MODE, desKey);
+			byte[] inputBytes = cipher.doFinal(stringToBeEncrypted.getBytes());
+			encryptedString = new String(Base64.encode(inputBytes));
+		} catch (InvalidKeyException e) {
+			LOGGER.error("InvalidKeyException while encrypting String:" + stringToBeEncrypted + " with Exception:" + e);
+		} catch (BadPaddingException e) {
+			LOGGER.error("BadPaddingException while encrypting String:" + stringToBeEncrypted + " with Exception:" + e);
+		} catch (IllegalBlockSizeException e) {
+			LOGGER.error("IllegalBlockSizeException while encrypting String:" + stringToBeEncrypted + " with Exception:"
+					+ e);
+		} catch (NoSuchAlgorithmException e) {
+			LOGGER.error(
+					"NoSuchAlgorithmException while encrypting String:" + stringToBeEncrypted + " with Exception:" + e);
+		} catch (NoSuchPaddingException e) {
+			LOGGER.error(
+					"NoSuchPaddingException while encrypting String:" + stringToBeEncrypted + " with Exception:" + e);
+		} catch (InvalidKeySpecException e) {
+			LOGGER.error(
+					"InvalidKeySpecException while encrypting String:" + stringToBeEncrypted + " with Exception:" + e);
+		}
+		return encryptedString;
+	}
+	
+	public String decrypt(String stringToBeDecrypted) {
+		String descryptedString = null;
+		try {
+			DESKeySpec dks = new DESKeySpec(this.APP_PRIVATE_KEY.getBytes());
+			SecretKeyFactory skf = SecretKeyFactory.getInstance(Utility.algorithm);
+			SecretKey desKey = skf.generateSecret(dks);
+			Cipher cipher = Cipher.getInstance(Utility.algorithm);
+			cipher.init(Cipher.DECRYPT_MODE, desKey);
+			byte[] recoveredBytes = cipher.doFinal(Base64.decode(stringToBeDecrypted.getBytes()));
+			descryptedString = new String(recoveredBytes);
+		} catch (InvalidKeyException e) {
+			LOGGER.error("InvalidKeyException while decrypting String:" + stringToBeDecrypted + " with Exception:" + e);
+		} catch (BadPaddingException e) {
+			LOGGER.error("BadPaddingException while decrypting String:" + stringToBeDecrypted + " with Exception:" + e);
+		} catch (IllegalBlockSizeException e) {
+			LOGGER.error("IllegalBlockSizeException while decrypting String:" + stringToBeDecrypted + " with Exception:"
+					+ e);
+		} catch (NoSuchAlgorithmException e) {
+			LOGGER.error(
+					"NoSuchAlgorithmException while decrypting String:" + stringToBeDecrypted + " with Exception:" + e);
+		} catch (NoSuchPaddingException e) {
+			LOGGER.error(
+					"NoSuchPaddingException while decrypting String:" + stringToBeDecrypted + " with Exception:" + e);
+		} catch (InvalidKeySpecException e) {
+			LOGGER.error(
+					"InvalidKeySpecException while decrypting String:" + stringToBeDecrypted + " with Exception:" + e);
+		}
+		return descryptedString;
+	}
+	
+	public String replaceText(String source, Map<String, String> replacers) {
+		for (String replacer : replacers.keySet()) {
+			String replacerText = replacers.get(replacer);
+			source = source.replace(replacer, replacerText);
+		}
+		return source;
 	}
 }
